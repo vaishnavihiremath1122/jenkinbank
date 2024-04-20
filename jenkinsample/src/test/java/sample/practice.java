6706e5c9b61f052e3b392f9100ddcc4662a3a76c
hiremathvaishnavi27@gmail.com <hiremathvaishnavi27@gmail.com>
2024-04-20 12:09:53 +0530

    first

---
+++ jenkinsample/src/test/java/sample/practice.java
@@ -0,0 +1,19 @@
+package sample;
+
+import org.testng.annotations.Test;
+
+public class practice {
+	
+	@Test
+	private void practice1() {
+		System.out.println("hiii");
+	}
+	
+	private void practice2() {
+		System.out.println("hiii");
+	}
+	
+	private void practice3() {
+		System.out.println("hiii");
+	}
+}
---
+++ jenkinsample/src/test/java/sample/sample1.java
@@ -0,0 +1,22 @@
+package sample;
+
+import org.testng.annotations.Test;
+
+public class sample1 {
+	
+	
+	@Test
+	
+	private void sample1() {
+		System.out.println("hiii");
+	}
+	
+	private void sample2() {
+		System.out.println("hiii");
+	}
+	
+	private void sample3() {
+		System.out.println("hiii");
+	}
+
+}
